1ca19c7ef2ef8e27be4a8855d7ca168c72a5a46e
ian <fishy90@gmail.com>
2023-09-25 00:51:43 +0800

    fix navbar highlighting and wafflemooncake location

--- src/data/navData.js
+++ src/data/navData.js
@@ -1,6 +1,6 @@
 const navData = [
   {name: 'Home', path: '/'},
-  {name: 'Library', path: '/library'},
+  {name: 'Library', path: '/library/'},
   {name: 'Resume', path: 'https://www.wafflemooncake.dev/cv', external: true},
 ]
 
